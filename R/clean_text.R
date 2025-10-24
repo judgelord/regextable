dd7d67a77f2399fab89b11b288210d2b9710becf
ShirlynDong <shirlyndong@gmail.com>
2025-10-23 22:15:30 -0400

    removed redundant function call

--- R/clean_text.R
+++ R/clean_text.R
@@ -9,7 +9,6 @@ clean_text <- function(FROM){
     stringr::str_replace_all("\n", " ") %>% # remove paragraph breaks
     stringr::str_replace_all(" , | ,|,", ", ") %>% # fix misplaced commas
     stringr::str_replace_all("\\.", " ") %>% #remove periods
-    stringr::str_squish() %>%   
     stringr::str_replace_all("\\s*,\\s*", ",") %>% #remove multiple spaces before and after commas
     stringr::str_replace_all(",{2,}", ",") %>% #removing commas that repeat
     stringr::str_replace_all(",",", ") %>% #sets space after each comma
