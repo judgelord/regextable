676a1e12b37b89a236aeb61de89db6bbe5b3f78c
ShirlynDong <shirlyndong@gmail.com>
2025-10-24 20:06:21 -0400

    merged clean columns into clean_helpers

--- R/clean_all_columns.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-library(stringr)
-#Applies clean_text to all character columns
-clean_all_columns <- function(df){
-  df %>%
-    mutate(across(where(is.character), ~clean_text(.x)))
-}
