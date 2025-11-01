b8bffe27be910df871242aef2699329782d769cf
ShirlynDong <shirlyndong@gmail.com>
2025-11-01 14:16:05 -0400

    added roxygen comments and functin header for dates method

---
+++ R/regex_flex.R
@@ -0,0 +1,11 @@
+#' @title Flexible Regex Application
+#' @description Apply user specific flexible regex_table methods
+#' @import dplyr
+#' @import stringr
+#' @import apply_regextable
+#' @export
+
+
+apply_regextable_date <- function(df, col_name, regex_table, date_start, date_end) {
+
+}
