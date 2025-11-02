68cc957151b39b56f696670a776ff97490d0b1af
ShirlynDong <shirlyndong@gmail.com>
2025-11-02 18:29:43 -0500

    remvoed repeat function call in clean_text

--- R/clean_text.R
+++ R/clean_text.R
@@ -13,7 +13,6 @@ clean_text <- function(FROM){
     stringr::str_remove('\\+') %>%
     stringr::str_remove("\u2014") %>% #em dash
     stringr::str_replace_all("\n", " ") %>% # remove paragraph breaks
-    stringr::str_replace_all(" , | ,|,", ", ") %>% # fix misplaced commas
     stringr::str_replace_all("\\.", " ") %>% #remove periods
     stringr::str_replace_all("\\s*,\\s*", ",") %>% #remove multiple spaces before and after commas
     stringr::str_replace_all(",{2,}", ",") %>% #removing commas that repeat
