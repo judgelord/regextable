4b2ab032e8519938a6827c7c7d0d9b30e119112a
ShirlynDong <shirlyndong@gmail.com>
2025-11-09 12:59:19 -0500

    implemented typo function

--- R/fix_typos.R
+++ R/fix_typos.R
@@ -6,4 +6,33 @@
 #' @return Character vector with replacements applied
 #' @export
 
-apply_typos <- function(x, table, verbose = TRUE)
+apply_typos <- function(x, table, verbose = TRUE){
+  if (is.null(table)) {
+    return(x)
+  }
+  
+  table <- as.data.frame(table, stringsAsFactors = FALSE)
+  if (ncol(table) != 2) {
+    stop("'table' must have exactly two columns: pattern and replacement")
+  }
+  
+  if (verbose) {
+    message("Apply user-supplied corrections...")
+    pb <- pbapply::startpb(0, nrow(table))
+  }
+  
+  for (i in seq_len(nrow(table))) {
+    x <- stringr::str_replace_all(x, table[[1]][i], table[[2]][i])
+    if (verbose) {
+      pbapply::setpb(pb, i)
+    }
+  }
+  
+  if (verbose) {
+    pbapply::closepb(pb)
+  }
+  
+  #remove extra spaces after replacements
+  x <- stringr::str_squish(x)
+  x
+}
