33323e47fcd212362201fd4db029b661dc4bccf6
ShirlynDong <shirlyndong@gmail.com>
2025-11-29 12:39:27 -0500

    updated test_clean_text.R

--- R/clean_text.R
+++ R/clean_text.R
@@ -9,8 +9,8 @@ clean_text <- function(text){
 
   text %>%
     stringr::str_to_lower() %>% #makes str lowercase for consistency
-    stringr::str_remove_all("[+–—\u2014!?:;\t]") %>% #removes special characters
-    stringr::str_replace_all("[\n.]", " ") %>% #replaces new lines and periods to one space
+    stringr::str_remove_all("[+\u2014!?:;]") %>% #removes punctuation
+    stringr::str_replace_all("[\n\t.-]", " ") %>% #replaces new lines, tabs, periods, hyphen, and dash with space
     stringr::str_replace("\\s*(?:,\\s*)+", ", ") %>% #replaces sequences and white space with a single ", "
     stringr::str_squish() # replace spaces with a single space
 }
--- tests/testthat/test-clean_text.R
+++ tests/testthat/test-clean_text.R
@@ -7,7 +7,12 @@ test_that("clean_text works as expected", {
   raw_text <- c(
     "  Hello,   World!  ",
     "R&D + Analysis — Test",
-    "Multiple\nlines.and.periods..."
+    "Multiple\nlines.and.periods...",
+    "already clean",
+    "",
+    "Multiple!!!! consecutive???? punctuation,,,,,, ",
+    "Hello\nWorld  ",
+    "hyphen- and Em—dash"
   )
   
   cleaned <- clean_text(raw_text)
@@ -16,5 +21,10 @@ test_that("clean_text works as expected", {
   expect_equal(cleaned[1], "hello, world")
   expect_equal(cleaned[2], "r&d analysis test")
   expect_equal(cleaned[3], "multiple lines and periods")
+  expect_equal(cleaned[4], "already clean")
+  expect_equal(cleaned[5], "")
+  expect_equal(cleaned[6], "multiple consecutive punctuation,")
+  expect_equal(cleaned[7], "hello world")
+  expect_equal(cleaned[8], "hyphen and emdash")
   
 })
